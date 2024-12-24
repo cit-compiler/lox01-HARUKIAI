ade0b46246a1ad232df3562e4301e7bcfdd5f930
HARUKIAI <cihr22051@g.nihon-u.ac.jp>
2024-12-24 10:05:49 +0900

    Parser completed

--- codes/com/craftinginterpreters/lox/Lox.java
+++ codes/com/craftinginterpreters/lox/Lox.java
@@ -43,7 +43,6 @@ public class Lox {
     private static void run(String source) {
       Scanner scanner = new Scanner(source);
       List<Token> tokens = scanner.scanTokens();
-    
       //For now, just print the tokens.
       for (Token token : tokens) {
         System.out.println(token);
